a3c01c8ff810a44b51a74405265fe36bdc2b1e34
Lukkas98 <lucaspalma988@gmail.com>
2024-04-06 14:45:19 -0300

    add font and next config

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import { Chakra_Petch } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const Font = Chakra_Petch({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Country page",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={Font.className}>{children}</body>
     </html>
   );
 }
