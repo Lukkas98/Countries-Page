25df2ce40e7c898298fd3ec1b1a256b5376df9ad
Lukkas98 <lucaspalma988@gmail.com>
2024-04-06 14:51:07 -0300

    fix font

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Chakra_Petch } from "next/font/google";
 import "./globals.css";
 
-const Font = Chakra_Petch({ subsets: ["latin"] });
+const Font = Chakra_Petch({ subsets: ["latin"], weight: ["400", "500", "600", "700"], style: ["normal"] });
 
 export const metadata = {
   title: "Country page",
